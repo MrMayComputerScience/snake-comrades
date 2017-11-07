89f977e744179fcdcfff7ef40a4fc984cfe75dc7
Zach <edgleyshotfirst@users.noreply.github.com>
2017-11-07 13:47:09 -0600

    Ooof
    better than Chris

---
+++ src/PlayButton.java
@@ -0,0 +1,21 @@
+/**
+ * @(#)PlayButton.java
+ *
+ *
+ * @author 
+ * @version 1.00 2017/5/1
+ */
+
+import mayflower.*;
+public class PlayButton extends Actor
+{
+    public PlayButton() 
+    {
+    	setImage("img/start.png");
+    }
+    
+    @Override
+    public void act()
+    {
+    }
+}
---
+++ src/ScoreBoard.java
@@ -0,0 +1,35 @@
+/**
+ * @(#)ScoreBoard.java
+ *
+ *
+ * @author 
+ * @version 1.00 2017/5/8
+ */
+
+import mayflower.*;
+public class ScoreBoard extends Actor
+{
+	private int score;
+    public ScoreBoard()
+    {
+
+    	score = 0;
+    	setImage(new MayflowerImage("Score: "+ score,16,new Color(0,255,0)));
+
+    }
+    public void plusScore()
+    {
+    	score++;
+    	act();
+    }
+    public int getScore()
+    {
+    	return score;
+    }
+    
+    @Override
+    public void act()
+    {
+    	setImage(new MayflowerImage("Score: "+ score,16,new Color(0,255,0)));
+    }
+}
