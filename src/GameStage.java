96df0dab2dfe605dec1c799c7285ae9607e8a150
Zach <edgleyshotfirst@users.noreply.github.com>
2017-11-07 14:15:49 -0600

    Snake stuff

--- src/GameStage.java
+++ src/GameStage.java
@@ -10,11 +10,16 @@ import mayflower.*;
 public class GameStage extends World
 {
 	private ScoreBoard s;
+	private Snake player;
+	private Random rand;
     public GameStage(ScoreBoard a)
     {
     	setBackground("img/blue.png");
     	s=a;
 		addObject(s, 50,50);
+		rand = new Random();
+		player = new Snake(100,100);
+		addObject(player,100,100);
     }
     
     @Override
---
+++ src/SnakePart.java
@@ -0,0 +1,13 @@
+import mayflower.*;
+public class SnakePart extends Actor
+{
+    public SnakePart()
+    {
+        setImage("img/snekpartbasic.png");
+    }
+
+    @Override
+    public void act()
+    {
+    }
+}
