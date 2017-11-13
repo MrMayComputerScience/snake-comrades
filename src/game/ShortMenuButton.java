c22cef1e692aec40274d3a6f1280d056b0e02637
Chris McLain <christopher@homerunss.com>
2017-11-13 14:03:47 -0600

    minecraft

---
+++ src/game/ShortMenuButton.java
@@ -0,0 +1,15 @@
+package game;
+
+import mayflower.Actor;
+
+public class ShortMenuButton extends Actor {
+
+    public ShortMenuButton() {
+        setImage("img/shortbutton.png");
+    }
+
+    @Override
+    public void act() {
+
+    }
+}
