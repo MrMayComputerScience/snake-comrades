c571541b1d11fdf8fffa1ba54eb6d7ab8669cb8b
Zach <edgleyshotfirst@users.noreply.github.com>
2017-11-15 13:49:49 -0600

    fix for menu

--- src/game/StartMenu.java
+++ src/game/StartMenu.java
@@ -42,13 +42,25 @@ public class StartMenu extends World
 
     public StartMenu(ScoreBoard a, KeyCounter j)
 	{
-		this();
+		setBackground("img/titlescreen_minecraft.png");
     	s = a;
     	k = j;
 		score = a.getScore();
 		keys = k.getScore();
     	addObject(s, 50,50);
     	addObject(k,50,65);
+
+		singleplayer = new LongMenuButton();
+		localMulti = new LongMenuButton();
+		netMulti = new LongMenuButton();
+		lootboxes = new ShortMenuButton();
+		quit = new ShortMenuButton();
+
+		addObject(singleplayer, 233, 246);
+		addObject(localMulti, 233, 246+48);
+		addObject(netMulti, 233, 246+48*2);
+		addObject(lootboxes, 233, 414);
+		addObject(quit, 403, 414);
     }
 
     @Override
