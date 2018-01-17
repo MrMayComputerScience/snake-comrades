4e0f38cac3cd33414c26e8205c4e39f2990c67ed
Chris McLain <christopher@homerunss.com>
2018-01-17 13:12:19 -0600

    Refactoring

--- src/game/Stages/Snake.java
+++ src/game/Stages/Snake.java
@@ -140,7 +140,7 @@ public class Snake extends Snakesque {
 
             //Updates
             this.setImage(img);
-            System.out.println("x=" + getX() + ",y=" + getY() + ",guid=" + (getSnakeGuid().toString().substring(0,4)));
+            System.out.println("x=" + getX() + ", y=" + getY() + ", guid=" + (getSnakeGuid().toString().substring(0,4)));
         }
     }
 
@@ -238,39 +238,3 @@ public class Snake extends Snakesque {
     }
 }
 
-enum Direction {
-    N, E, S, W
-}
-
-enum ControlScheme {
-    WASD(Keyboard.KEY_W, Keyboard.KEY_A, Keyboard.KEY_S, Keyboard.KEY_D),
-    ARROWS(Keyboard.KEY_UP, Keyboard.KEY_LEFT, Keyboard.KEY_DOWN, Keyboard.KEY_RIGHT),
-    TFGH(Keyboard.KEY_T, Keyboard.KEY_F, Keyboard.KEY_G, Keyboard.KEY_H),
-    IJKL(Keyboard.KEY_I, Keyboard.KEY_J, Keyboard.KEY_K, Keyboard.KEY_L),
-    ;
-
-    private int up, left, down, right;
-
-    ControlScheme(int up, int left, int down, int right) {
-        this.up = up;
-        this.left = left;
-        this.down = down;
-        this.right = right;
-    }
-
-    public int getUp() {
-        return up;
-    }
-
-    public int getLeft() {
-        return left;
-    }
-
-    public int getDown() {
-        return down;
-    }
-
-    public int getRight() {
-        return right;
-    }
-}
